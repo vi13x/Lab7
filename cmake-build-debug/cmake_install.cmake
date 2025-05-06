bc31196324893b426978714d7061fd0337ea1ee4
vi13x <lakotko.vlad@mail.ru>
2025-05-06 20:43:48 +0300

    Add files via upload

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Vladislav Lakotko/CLionProjects/LABA6_2sem
+# Install script for directory: C:/Users/Vladislav Lakotko/CLionProjects/LABA7_2sem
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/LABA6_2sem")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/LABA7_2sem")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Vladislav Lakotko/CLionProjects/LABA6_2sem/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Vladislav Lakotko/CLionProjects/LABA7_2sem/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
